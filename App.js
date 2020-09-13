4033518ce37b3a00aac66669160c7b3019890acf
whitefang-asus <sankitpramod.min18@itbhu.ac.in>
2020-09-13 23:36:17 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,35 @@
+const pet = ({name,animal,bread}) => {
+    return React.createElement("div",{},
+        [
+            React.createElement('h1',{},name),
+            React.createElement('h2',{},animal),
+            React.createElement('h2',{},bread)
+        ]
+    )
+}
+
+
+
+const App = () =>{
+    return React.createElement("div",{},
+        [
+            React.createElement("h1",{},"Adopt Me!"),
+            React.createElement(pet,{
+                name:"john",
+                animal:"cat",
+                bread: "Russian cat"
+            }),
+            React.createElement(pet,{
+                name:"hamm",
+                animal:"horse",
+                bread: "pony"
+            }),
+            
+            
+        ]
+
+    );
+};
+
+ReactDOM.render(React.createElement(App),document.getElementById("root"));
+
