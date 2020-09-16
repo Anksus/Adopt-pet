a035af918b2fcaa73d5422bfee5d71900b995681
whitefang-asus <sankitpramod.min18@itbhu.ac.in>
2020-09-17 00:08:52 +0530

    added new route

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
 import React from "react";
 import { render } from "react-dom";
-// import Pet from "./pet";
+import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
-  //   return React.createElement("div", {}, [
-  //     React.createElement("h1", {}, "Adopt Me!"),
-  //     React.createElement(Pet, {
-  //       name: "john",
-  //       animal: "cat",
-  //       bread: "Russian cat",
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "hamm",
-  //       animal: "horse",
-  //       bread: "pony",
-  //     }),
-  //   ]);
-
   // <div> basically means createElement with div in it in JSX
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      {/* <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" /> */}
-      <SearchParams />
+      <header>
+        <Link to="/">
+          <h1>Adopt Me!</h1>
+        </Link>
+      </header>
+
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </div>
   );
 };
---
+++ src/Details.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Details = () => {
+  return <h1>hii lol!</h1>;
+};
+
+export default Details;
