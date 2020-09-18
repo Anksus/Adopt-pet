ed0dda69e750373cb7a48ed0b4027e3bcf4e7c0b
whitefang-asus <sankitpramod.min18@itbhu.ac.in>
2020-09-18 23:28:53 +0530

    added ErrorBoundary and auto redirecting functionality

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link, Redirect, redirectTo } from "@reach/router";
+import { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, redirect: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error("ErrorBoundary caught an error", error, info);
+  }
+
+  componentDidUpdate() {
+    if (this.state.hasError) {
+      setTimeout(() => this.setState({ redirect: true }), 5000);
+    }
+  }
+
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to="/" noThrow />;
+    }
+
+    if (this.state.hasError) {
+      return (
+        <h1 style={{ color: "white" }}>
+          There was an error with this listing.
+          <Link to="/" style={{ color: "white" }}>
+            Click here
+          </Link>
+          . To back to the home page or wait five seconds.
+        </h1>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
